f833f36949c64268d6f16ce4d2e1595b07b6fd39
21MoududAhammed <21moudud@gmail.com>
2024-08-18 08:56:43 +0600

    Header added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Brands from "./components/Brands";
 // import FeaturedWork from "./components/FeaturedWork";
 import Footer from "./components/Footer";
+import Header from "./components/Header";
 import PositiveIntroduction from "./components/PositiveIntroduction";
 import Pricing from "./components/Pricing";
 
@@ -8,7 +9,7 @@ export default function App() {
   return (
     <>
       {/* <FeaturedWork /> */}
-
+      <Header/>
       <Brands />
       <PositiveIntroduction />
       <Pricing />
